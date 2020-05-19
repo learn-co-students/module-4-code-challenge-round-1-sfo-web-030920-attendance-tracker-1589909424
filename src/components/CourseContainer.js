c98617162089d5c555f8d00bef0ca845d447c43d
caicindy87 <caicindy87@gmail.com>
2020-05-19 11:55:02 -0700

    Completed code challenge

--- src/components/CourseContainer.js
+++ src/components/CourseContainer.js
@@ -1,15 +1,87 @@
 import React, { Component } from "react";
+
 import CourseDetails from "./CourseDetails";
 import CourseSelector from "./CourseSelector";
 import StudentsList from "./StudentsList";
 
 class CourseContainer extends Component {
+  state = {
+    courses: [],
+    students: [],
+    selectedCourse: "",
+  };
+
+  componentDidMount() {
+    fetch("http://localhost:6001/courses")
+      .then((resp) => resp.json())
+      .then((data) =>
+        this.setState({
+          courses: data,
+        })
+      );
+
+    fetch("http://localhost:6001/students")
+      .then((resp) => resp.json())
+      .then((data) =>
+        this.setState({
+          students: data,
+        })
+      );
+  }
+
+  handleCourseChange = (e) => {
+    const courseIDInt = parseInt(e.target.value);
+    const selectedCourse = this.state.courses.find(
+      (course) => course.id === courseIDInt
+    );
+
+    this.setState({
+      selectedCourse: selectedCourse,
+    });
+  };
+
+  changeCourse = () => {
+    return this.state.students.filter(
+      (student) => student.course === this.state.selectedCourse.name
+    );
+  };
+
+  toggleAttendingCheckbox = (toggledStudent) => {
+    const updatedStudents = this.state.students.map((student) =>
+      student.id === toggledStudent.id
+        ? { ...toggledStudent, attending: !toggledStudent.attending }
+        : student
+    );
+
+    this.setState({
+      students: updatedStudents,
+    });
+
+    fetch(`http://localhost:6001/students/${toggledStudent.id}`, {
+      method: "PATCH",
+      body: JSON.stringify({
+        attending: !toggledStudent.attending,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  };
+
   render() {
+    const { courses } = this.state;
+
     return (
       <div className="ui grid container">
-        <CourseDetails />
-        <CourseSelector />
-        <StudentsList />
+        <CourseDetails course={this.state.selectedCourse} />
+        <CourseSelector
+          courses={courses}
+          handleCourseChange={this.handleCourseChange}
+        />
+        <StudentsList
+          students={this.changeCourse()}
+          toggleAttendingCheckbox={this.toggleAttendingCheckbox}
+        />
       </div>
     );
   }
--- src/components/Student.js
+++ src/components/Student.js
@@ -1,15 +1,17 @@
 import React from "react";
 
-const Student = () => (
+const Student = ({ student, toggleAttendingCheckbox }) => (
   <tr style={{ textAlign: "center" }}>
-    <td>"...your code here"</td>
-    <td>"...your code here"</td>
-    <td>"...your code here"</td>
+    <td>{student.name}</td>
+    <td>{student.class_year}</td>
+    <td>{student.percentage}</td>
     <td>
       <input
         type="checkbox"
-        checked={null /* if true, this checkbox will be checked! */}
-        onClick={() => console.log("You clicked me!")}
+        checked={
+          student.attending /* if true, this checkbox will be checked! */
+        }
+        onClick={() => toggleAttendingCheckbox(student)}
       />
     </td>
   </tr>
