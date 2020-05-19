cfff3cc71f5f768332a9a1a2b13977fc4d4105bf
SaberChulainn <Tbandachit@gmail.com>
2020-05-19 11:16:03 -0700

    Finished first part

--- src/components/CourseContainer.js
+++ src/components/CourseContainer.js
@@ -4,12 +4,41 @@ import CourseSelector from "./CourseSelector";
 import StudentsList from "./StudentsList";
 
 class CourseContainer extends Component {
+  state = {
+    students: [],
+    courseName: [],
+    course: ''
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:6001/students')
+    .then(resp => resp.json())
+    .then(data => this.setState({
+      students: data
+    }))
+
+    fetch('http://localhost:6001/courses')
+    .then(resp => resp.json())
+    .then(data => this.setState({
+      courseDetails: data
+    }))
+  }
+  handleChange = (e) => {
+    this.setState({
+      course: e.target.value
+    })
+  }
+
+  filterStudents = () => {
+    console.log(this.state.course)
+    return this.state.students.filter(student => student.course == this.state.course)
+  }
   render() {
     return (
       <div className="ui grid container">
         <CourseDetails />
-        <CourseSelector />
-        <StudentsList />
+        <CourseSelector courses={this.state.courseDetails} handleChange={this.handleChange}/>
+        <StudentsList students={this.filterStudents()}/>
       </div>
     );
   }
